438b8f18b1d6be17a24da6255da1298d6fa37c25
Jeremy CHAZELLE <jeremy.chazelle2022@campus-eni.fr>
2022-05-31 15:00:26 +0200

    Redirection vers accueil et modif SortieController

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,17 +2,55 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
-    #[Route('/', name: 'app_sortie')]
-    public function home(): Response
+    #[Route('/', name: 'accueil')]
+    public function accueil(SortieRepository $sortieRepository): Response
+    {
+        $sortie = $sortieRepository->findAll();
+        return $this->render('sortie/accueil.html.twig', [
+            'sortie' => $sortie,
+        ]);
+    }
+
+    #[Route('/add', name: 'addSortie')]
+    public function add(): Response
     {
         return $this->render('sortie/accueil.html.twig', [
             'controller_name' => 'SortieController',
         ]);
     }
+
+    #[Route('/display{id}', name: 'displaySortie')]
+    public function display($id, SortieRepository $sortieRepository): Response
+    {
+        $sortie = $sortieRepository->find($id);
+
+        return $this->render('sortie/accueil.html.twig', [
+            'id' => $id,
+            'sortie' => $sortie,
+        ]);
+    }
+
+//    #[Route('/update', name: 'updateSortie')]
+//    public function update(): Response
+//    {
+//        return $this->render('sortie/accueil.html.twig', [
+//            'controller_name' => 'SortieController',
+//        ]);
+//    }
+//
+//    #[Route('/cancel', name: 'cancelSortie')]
+//    public function cancel(): Response
+//    {
+//        return $this->render('sortie/accueil.html.twig', [
+//            'controller_name' => 'SortieController',
+//        ]);
+//    }
+
 }
