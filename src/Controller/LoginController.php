7e24961067e6df1b82bef8922e1c8e9406f0b2af
Qdelys <quentin.delys2022@campus-eni.fr>
2022-06-01 16:57:02 +0200

    modification profil V3

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -31,5 +31,6 @@ class LoginController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+
     }
 }
--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -5,17 +5,17 @@ namespace App\Form;
 use App\Entity\Participant;
 
 
-use Cassandra\Blob;
-use Doctrine\DBAL\Types\BlobType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 
 class ParticipantType extends AbstractType
@@ -23,52 +23,63 @@ class ParticipantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username',TextType::class,[
+            ->add('username', TextType::class, [
                 'label' => "Pseudo : ",
                 'attr' => [
                     'placeholder' => 'Peusdo....',]
-                    ])
-            ->add('prenom',TextType::class,[
+            ])
+            ->add('prenom', TextType::class, [
                 'label' => "Prénom : ",
                 'attr' => [
                     'placeholder' => 'Prénom....',]
             ])
-            ->add('nom',TextType::class,[
+            ->add('nom', TextType::class, [
                 'label' => "Nom : ",
                 'attr' => [
                     'placeholder' => 'Nom....',]
             ])
-            ->add('telephone', TextType::class,[
+            ->add('telephone', TextType::class, [
                 'label' => "Téléphone :",
                 'attr' => [
                     'placeholder' => '0685649568',]
             ])
-            ->add('email', EmailType::class,[
+            ->add('email', EmailType::class, [
                 'label' => "Email : ",
                 'required' => false,
                 'attr' => [
                     'placeholder' => 'email@Eni.fr',]
             ])
-            ->add('password', PasswordType::class,[
-                'label' => "Mot de passe :",
-                'required' => false,
-            ])
-            ->add('confPassword', PasswordType::class,[
-                'label' => "Confirmation :",
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
                 'required' => false,
+                'invalid_message' => false,
+
+                'first_options'  => ['label' => 'Mot de passe :'],
+                'second_options' => ['label' => 'Confirmation :'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe ne respect pas le minimum de caractères (6)',
+                        'max' => 100,
+                        'maxMessage' => 'Votre mot de passe ne respect pas le maximun de caractères (100)'
+                    ])
+                ]
             ])
-            ->add('campus',EntityType::class,[
+
+            ->add('campus', EntityType::class, [
                 'label' => 'Campus :',
                 'choice_label' => "nom",
                 'class' => 'App\Entity\Campus'
             ])
-            ->add('photo', FileType::class,[
+            ->add('photo', FileType::class, [
                 'label' => "Ma photo :",
                 'mapped' => false,
                 'required' => false,
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
