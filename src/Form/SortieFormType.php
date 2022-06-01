1f52409636adb6b08db6cb54b198a85460610718
Jeremy CHAZELLE <jeremy.chazelle2022@campus-eni.fr>
2022-06-01 15:30:24 +0200

    Travail sur la page accueil + création du folder Model avec la classe Search.php et "transfert" sur ListSortiesType. des bisous !

--- src/Form/SortieFormType.php
+++ src/Form/SortieFormType.php
@@ -1,9 +1,11 @@
 <?php
-
 namespace App\Form;
 
 use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,62 +16,69 @@ class SortieFormType extends AbstractType
     {
         $builder
             ->add('nom', TextType::class,[
-                'label' => 'Le nom de la sortie contient :',
+                'label' => 'Nom de la sortie',
                 'required' => true
             ])
-            ->add('dateHeureDebut', TextType::class,[
-                'label' => 'Entre ',
+            ->add('dateHeureDebut', DateTimeType::class,[
+                'label' => 'Date et heure de début',
                 'required' => true
             ])
-            ->add('duree', TextType::class,[
-                'label' => 'Durée de la sortie',
-                 'required' => true
-            ])
-            ->add('dateLimiteInscription', TextType::class,[
+            ->add('dateLimiteInscription', DateTimeType::class,[
                 'label'=> 'Date limite d\'inscription',
                 'required' => true
             ])
             ->add('nbInscriptionMax', TextType::class,[
                 'label' => 'Nombre max d\'inscrits',
-                            'required' => true
-            ])
-            ->add('infosSortie', TextType::class,[
-                'label' => 'Informations de la sortie',
-                'required' => true
-            ])
-            ->add('organisateur', TextType::class,[
-                'label' => 'Organisateur de la sortie',
-                'required' => true
-            ])
-            ->add('participants', TextType::class,[
-                'label' => 'Participants de la sortie',
-                'required' => true,
-                'mapped' => false
-            ])
-            ->add('campus', TextType::class,[
-                'label' => 'Campus de la sortie',
-                'required' => true
-            ])
-            ->add('lieu', TextType::class,[
-                'label' => 'Lieu de la sortie',
-                'required' => true
-            ])
-            ->add('etat', TextType::class,[
-                'label' => 'Etat de la sortie',
-                'required' => true
-            ])
-            ->add('date', TextType::class,[
-                'label' => 'Etat de la sortie',
                 'required' => true
             ])
 
+            ->add('infosSortie', TextareaType::class, [
+                'label' => "Informations de la sortie",
+                'attr' => [
+                    'class' => "descText"
+                ]
+            ])
+            ->add('Campus', EntityType::class,[
+                'label' => 'Campus de la sortie',
+                'choice_label' => "nom",
+                'class'=>'App\Entity\Campus',
+                'required' => true
+            ])
+            ->add('Lieu', EntityType::class,[
+                'label' => 'Campus de la sortie',
+                'choice_label' => "nom",
+                'class'=>'App\Entity\Lieu',
+                'required' => true
+            ])
+            ->add('Etat', EntityType::class,[
+                'label' => 'Campus de la sortie',
+                'choice_label' => "nom",
+                'class'=>'App\Entity\Etat',
+                'required' => true
+            ])
+            ->add('rue', TextType::class,[
+                'label' => 'rue',
+                'required' => true, 'mapped'=> false
+            ])
+            ->add('codePostal', TextType::class,[
+                'label' => 'codePostal',
+                'required' => true, 'mapped'=> false
+            ])
+            ->add('latitude', TextType::class,[
+                'label' => 'latitude',
+                'required' => true, 'mapped'=> false
+            ])
+            ->add('longitude', TextType::class,[
+                'label' => 'longitude',
+                'required' => true, 'mapped'=> false
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => null,
+            'data_class' => Sortie::class,
         ]);
     }
 }
