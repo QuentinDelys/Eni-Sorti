077b50162bf5e96d54b008b18c0ba1e3ec973692
Jeremy CHAZELLE <jeremy.chazelle2022@campus-eni.fr>
2022-06-09 12:17:40 +0200

    Fin de travail sur page d'accueil : filtres OK , Bouttons OK, Actions Nearly OK ON VA Y ARRIVER !

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -7,9 +7,7 @@ use App\Entity\Sortie;
 use App\Form\ListSortiesType;
 use App\Form\Model\Search;
 use App\Form\SortieFormType;
-use App\Repository\CampusRepository;
 use App\Repository\EtatRepository;
-use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -162,42 +160,46 @@ class SortieController extends AbstractController
         return $this->redirectToRoute("sortie_accueil");
     }
 
-//    #[Route('/publier', 'publier')]
-//    public function publier(SortieRepository $sortieRepository, EtatRepository $etatRepository){
-//
-//        $sortie = new Sortie();
-//        $user = $this->getUser();
-//
-//        $sortie->setOrganisateur($user);
-//        $sortie->setEtat($etatRepository->findOneBy(array('libelle' => 'Ouverte')));
-//        $sortie->setCampus($user->getCampus());
-//
-//        $sortieRepository->add($sortie, true);
-//        $this->addFlash("success", "sortie ajoutée avec succès !");
-//        return $this->redirectToRoute("sortie_accueil");
-//        }
-//
-//
-//    #[Route('/inscription', 'inscription')]
-//    public function inscrire($id, SortieRepository $sortieRepository) : Response
-//    {
-//        $user = $this->getUser();
-//        $sortieRepository->find($user);
-//
-//
-//
-//        $this->addFlash("success", "Participant ajouté à la liste des participants !");
-//        return $this->redirectToRoute("sortie_accueil");
-//    }
-//
-//    #[Route('/desister', 'desister')]
-//    public function desistement($id, ParticipantRepository $participantRepository) : Response
-//    {
-//        $participant = $participantRepository->find($id);
-//        $participantRepository->remove($participant, true);
-//
-//        $this->addFlash("success", "Participant retiré de la liste des participants !");
-//        return $this->redirectToRoute("sortie_accueil");
-//    }
+    #[Route('/publier', name: 'publier')]
+    public function publier(SortieRepository $sortieRepository, EtatRepository $etatRepository){
+
+        $sortie = new Sortie();
+        $user = $this->getUser();
+
+        $sortie->setOrganisateur($user);
+        $sortie->setEtat($etatRepository->findOneBy(array('libelle' => 'Ouverte')));
+        $sortie->setCampus($user->getCampus());
+
+        $sortieRepository->add($sortie, true);
+        $this->addFlash("success", "sortie ajoutée avec succès !");
+        return $this->redirectToRoute("sortie_accueil");
+        }
+
+
+    #[Route('/inscription/{id}', name: 'inscription')]
+    public function inscrire($id, SortieRepository $sortieRepository) : Response
+    {
+        $user = $this->getUser();
+
+        $sortie = $sortieRepository->find($id);
+        $sortie->getParticipants()->add($user);
+        $sortieRepository->add($sortie, true);
+
+        $this->addFlash("success", "Participant ajouté à la liste des participants !");
+        return $this->redirectToRoute("sortie_accueil");
+    }
+
+    #[Route('/desister/{id}', name: 'desister')]
+    public function desistement($id, SortieRepository $sortieRepository) : Response
+    {
+        $user = $this->getUser();
+
+        $sortie = $sortieRepository->find($id);
+        $sortie->getParticipants()->remove($user);
+        $sortieRepository->add($sortie, true);
+
+        $this->addFlash("success", "Participant supprimé de la liste des participants !");
+        return $this->redirectToRoute("sortie_accueil");
+    }
 
 }
